4622dfd827e635d9ff5e76c43e3083aa60fd0393
rajaryan45 <rajaryanbehera@gmail.com>
2025-06-14 20:08:25 +0530

    update

---
+++ DsaComeback/src/com/dsa/Main.java
@@ -0,0 +1,9 @@
+package com.dsa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello Raj");
+	}
+
+}
---
+++ DsaComeback/src/com/linkedlistbase/Main.java
@@ -0,0 +1,21 @@
+package com.linkedlistbase;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		try {
+
+			
+			System.out.println("hello");
+
+//			for(char x[]: res) {
+//				for(char k : x)
+//					System.out.print(k+" ");
+//				System.out.println(' ');
+//			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}

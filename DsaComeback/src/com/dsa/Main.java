70fc2819b59286be254416de6704ca45cbd07995
rajaryan45 <rajaryanbehera@gmail.com>
2025-06-15 01:03:33 +0530

    twoSum

--- DsaComeback/src/com/dsa/Main.java
+++ DsaComeback/src/com/dsa/Main.java
@@ -1,9 +1,14 @@
 package com.dsa;
 
+import com.dsa.hash.TwoSums;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello Raj");
+		TwoSums twoSums = new TwoSums();
+		int[] res = twoSums.twoSum(new int[] {2,7,11,15}, 9);
+		System.out.println(res.toString());
 	}
 
 }
